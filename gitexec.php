d1dd94521247f2e5d4081ab19bb0f18e22b17fab
Gauthier <mysterandco@gmail.com>
2018-04-03 12:24:39 +0200

    Updated

--- gitexec.php
+++ gitexec.php
@@ -4,12 +4,12 @@
 
 
 $git_add = exec("git add .", $o);
-print("Git add: {$o[1]}". PHP_EOL);
+print("Git add: {$o[0]}". PHP_EOL);
 
 $git_commit = exec("git commit -m 'Updated'");
 print("Git commit: {$git_commit}". PHP_EOL);
 
-$git_push = exec("git push origin master");
-print("Git push origin: {$git_push}". PHP_EOL);
+$git_push = exec("git push origin master", $o);
+print("Git push origin: {$o[0]}". PHP_EOL);
 
 ?>
