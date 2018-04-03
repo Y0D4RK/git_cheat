052a219b36ced33b67667ce6761003a7c7b366c1
Gauthier <mysterandco@gmail.com>
2018-04-03 13:59:02 +0200

    Updated

--- gitexec.php
+++ gitexec.php
@@ -1,15 +1,11 @@
 <?php
 
+$file = file("./app.html");
 
 
 
-$git_add = exec("git add .", $o);
-print("Git add: {$o[0]}". PHP_EOL);
-
-$git_commit = exec("git commit -m 'Updated'");
-print("Git commit: {$git_commit}". PHP_EOL);
-
-$git_push = exec("git push origin master", $o);
-print("Git push origin: {$o[0]}". PHP_EOL);
+exec("git add .");
+exec("git commit -m 'Updated'");
+exec("git push origin master");
 
 ?>
